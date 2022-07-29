e3a65432048432a845aadfee2449b2369b009d7e
Kelvyn King <kelvyn.m.king@gmail.com>
2022-07-29 15:10:17 +1000

    Created .create  method.

--- lib/student.rb
+++ lib/student.rb
@@ -47,4 +47,11 @@ class Student
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
+
+  def self.create(name, grade)
+   student = self.new(name, grade)
+   student.save
+  end
+
+
 end
