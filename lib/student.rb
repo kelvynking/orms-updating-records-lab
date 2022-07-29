41d092690ddb95014b4c94d78bd19c073bfde070
Kelvyn King <kelvyn.m.king@gmail.com>
2022-07-29 14:49:45 +1000

    Created .create_table method.

--- lib/student.rb
+++ lib/student.rb
@@ -14,4 +14,15 @@ class Student
     @id = id
   end
 
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
 end
